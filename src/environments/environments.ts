2df2f1e6e56e6e962f07e49256391554446c0967
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-11-24 22:49:23 -0300

    refactor: configurando deploy

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://avalia-app-production.up.railway.app',
+  apiUrl: 'https://avalia-app-production.up.railway.app/api',
   //apiUrl: 'http://localhost:8080/api',
 };
