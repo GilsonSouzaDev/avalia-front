48310b94729c824f6fed06ada48c923424b0acf3
Gilson Souza <158112954+GilsonSouzaDev@users.noreply.github.com>
2025-11-24 23:10:11 -0300

    Fix apiUrl formatting in environments.ts
    testando

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://avalia-app-production.up.railway.app/api',
   //apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://avalia-app-production.up.railway.app/api',
  
 };
