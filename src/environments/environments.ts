c19873071468823c3adee46671cbc1282bc91bf9
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-12-27 22:21:10 -0300

    atualizando config deploy

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://avalia-app-production.up.railway.app/api',
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://avalia-app-v1-0.onrender.com/api',
+  //apiUrl: 'http://localhost:8080/api',
 
 };
